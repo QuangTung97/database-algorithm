d196611f27ef110b53699d11c8032e5a37aa1f48
quangtung97 <quangtung29121997@gmail.com>
2018-01-27 09:13:04 +0700

    Sudo to false

--- build_google_mock.sh
+++ build_google_mock.sh
@@ -1,17 +1,19 @@
 WORKING_DIR=$(pwd)
+CXX=g++-5
 
 git clone https://github.com/google/googletest
 cd googletest
 git checkout release-1.8.0
 GTEST_DIR=googletest
 GMOCK_DIR=googlemock
-g++-5 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
+
+$CXX -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
     -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} \
     -pthread -c ${GTEST_DIR}/src/gtest-all.cc
-g++-5 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
+$CXX -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
     -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} \
     -pthread -c ${GMOCK_DIR}/src/gmock-all.cc
-g++-5 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
+$CXX -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
     -isystem ${GMOCK_DIR}/include -I${GMOCK_DIR} \
     -pthread -c ${GMOCK_DIR}/src/gmock_main.cc
 ar -rv libgmock_main.a gtest-all.o gmock-all.o gmock_main.o
